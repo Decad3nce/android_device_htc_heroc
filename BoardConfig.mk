754ccca02308ceccefa7c06f8ed15b68d94ebe2d
Decad3nce <Decad3nce@tweaktool.com>
2011-01-18 20:28:37 -0600

    Reorganize BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,15 +21,19 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=\$(TOP)/device/htc/heroc/prelink-linux-arm-heroc.map
+
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
 -include vendor/htc/heroc/BoardConfigVendor.mk
 
-TARGET_NO_BOOTLOADER := true
-
 TARGET_BOARD_PLATFORM := msm7k
 TARGET_BOARD_PLATFORM_GPU := qcom
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RECOVERY := true
 
 # ARMv6-compatible processor rev 5 (v6l)
 TARGET_CPU_ABI := armeabi-v6l
@@ -50,6 +54,11 @@ BOARD_USES_GENERIC_AUDIO := false
 
 # Use HTC USB Function Switch to enable tethering via USB
 BOARD_USE_HTC_USB_FUNCTION_SWITCH := true
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+
+BOARD_USE_HTC_LIBSENSORS := true
+BOARD_USE_HERO_LIBSENSORS := true
+BOARD_USES_QCOM_LIBS := true
 
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 BOARD_KERNEL_BASE := 0x19200000
@@ -62,9 +71,15 @@ BOARD_VENDOR_USE_AKMD := akm8973
 
 BOARD_VENDOR_QCOM_AMSS_VERSION := 4410
 
-BOARD_USES_QCOM_LIBS := true
+# Use RGB565 surfaces until userspace drivers are upgraded
+BOARD_NO_RGBX_8888 := true
+
+# Use Eclair libcamera
 
 BOARD_USES_ECLAIR_LIBCAMERA := true
+
+# use old sensors HAL
+TARGET_USES_OLD_LIBSENSORS_HAL := true
 
 # # cat /proc/mtd
 # dev:    size   erasesize  name
@@ -80,10 +95,13 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0aa00000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x09f80000
 # The size of a block that can be marked bad.
 BOARD_FLASH_BLOCK_SIZE := 131072
+#BOARD_CAMERA_LIBRARIES := libcameraservice libcamera
 
-TARGET_RECOVERY_UI_LIB := librecovery_ui_heroc librecovery_ui_generic
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_heroc librecovery_ui_generic
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 
-PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=\$(TOP)/device/htc/heroc/prelink-linux-arm-heroc.map
+#Use v8 JS engine
+JS_ENGINE := v8
+
 TARGET_PREBUILT_KERNEL := device/htc/heroc/kernel
